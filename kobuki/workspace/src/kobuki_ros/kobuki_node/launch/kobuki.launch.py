954cdff6c1d6f5cfa7b2413ab71b01e2c8c03cff
Vladislav <m.v.of@ya.ru>
2024-10-08 13:05:47 +0300

    /odom -> /master/odom

--- kobuki/workspace/src/kobuki_ros/kobuki_node/launch/kobuki.launch.py
+++ kobuki/workspace/src/kobuki_ros/kobuki_node/launch/kobuki.launch.py
@@ -13,10 +13,12 @@ def generate_launch_description():
     params_file = os.path.join(share_dir, 'config', 'kobuki_node_params.yaml')
     with open(params_file, 'r') as f:
         params = yaml.safe_load(f)['kobuki_ros_node']['ros__parameters']
+    remappings = [('/odom', '/master/odom')]
 
     kobuki_ros_node = Node(package='kobuki_node',
                            executable='kobuki_ros_node',
                            output='screen',
-                           parameters=[params])
+                           parameters=[params],
+                           remappings=remappings)
 
     return launch.LaunchDescription([kobuki_ros_node])
