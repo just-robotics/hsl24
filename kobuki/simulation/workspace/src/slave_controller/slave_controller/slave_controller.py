04f882c5ee0e264b9eba2cef771ddf068c7c0b9f
Artem Kondratev <artemkondratev5@gmail.com>
2024-10-10 10:21:38 +0300

    latest update Co-authored-by: AntonPolyTech <AntonPolyTech@users.noreply.github.com>

--- kobuki/simulation/workspace/src/slave_controller/slave_controller/slave_controller.py
+++ kobuki/simulation/workspace/src/slave_controller/slave_controller/slave_controller.py
@@ -4,8 +4,8 @@ import numpy as np
 import tf2_ros
 import tf_transformations
 import math
+import time
 
-from time import time
 from rclpy.node import Node
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image, CameraInfo
@@ -15,6 +15,7 @@ from tf2_ros import TransformException, TransformBroadcaster
 from geometry_msgs.msg import Quaternion, PointStamped, Vector3, TransformStamped, Twist, PoseStamped, Point
 from nav_msgs.msg import Odometry
 from std_msgs.msg import Header, Bool
+from std_msgs.msg import Float64
 
 
 class SlaveController(Node):
@@ -22,209 +23,211 @@ class SlaveController(Node):
     def __init__(self):
         super().__init__('slave_controller')
     
-        self.create_subscription(PoseStamped, '/slave/target', self.controller_callback, 10)
-        self.create_subscription(Odometry, '/slave/odom', self.sl_odom_callback, 10)
-        self.create_subscription(Odometry, '/master/odom', self.mas_odom_callback, 10)
+        self.create_subscription(Odometry, '/slave/odom', self.odom_callback, 10)
         self.create_subscription(PoseStamped, '/slave/origin', self.slave_origin_callback, 10)
         self.create_subscription(Bool, '/slave_ride_permission', self.slave_ride_permission_callback, 10)
+        self.create_subscription(Bool, '/aruco/found', self.aruco_callback, 10)
+        self.create_subscription(Float64, '/aruco_pkg/delta_angle', self.delta_callback, 10)
+        self.create_subscription(Bool, '/slave/start_rotation', self.start_rot_callback, 10)
+        self.create_subscription(Bool, '/slave/start_translation', self.start_tran_callback, 10)
+        self.create_subscription(PoseStamped, '/master/state', self.master_state_callback, 10)
 
-        self.velocity_topic = '/slave/cmd_vel'
-        self.point_topic = '/slave/goal_pose'
-        self.master_gp_topic = '/goal_pose'
-        self.slave_odom_topic = '/slave/odom'
+        self.cmd_vel_pub = self.create_publisher(Twist, '/slave/cmd_vel', 10)
+        self.goal_pose_pub = self.create_publisher(PoseStamped, '/slave/odom/goal_pose', 10)
+        self.rot_pub = self.create_publisher(Bool, '/slave/rotated', 10)
+        self.tran_pub = self.create_publisher(Bool, '/slave/translated', 10)
+        
+        self.create_timer(0.005, self.rotation_callback)
+        self.create_timer(0.01, self.translation_callback)
+        
+        self.get_logger().info('node started')
 
-        self.vel_publisher = self.create_publisher(Twist, self.velocity_topic, 10)
-        self.point_publisher = self.create_publisher(PointStamped, self.point_topic, 10)
-        self.gp_publisher = self.create_publisher(PoseStamped, self.master_gp_topic, 10)
-        self.reached_gp_flag_publisher = self.create_publisher(PoseStamped, '/slave_target_is_reached', 10)
-
-        self.max_linear_vel = 0.10
-        self.max_angular_vel = 0.70
-        self.ANGLE_ERR = 3  # degrees
+        self.ANGLE_ERR = 5  # degrees
         self.LINEAR_ERR = 0.05  # meters
-        self.base_footprint_slave_link = 'base_footprint_slave'
-        self.map_link = 'map'
+        
+        self.base_footprint_slave_frame = 'base_footprint_slave'
+        self.map_frame = 'map'
+        self.odom_slave_frame = 'odom_slave'
 
-        self.gp_vector = PoseStamped()
-        self.slave_odom = Odometry()
-        self.sl_odom_origin = PoseStamped()
-        self.slave_odom_point = PointStamped()
-        self.slave_ride_permission = False
+        self.goal_pose = PoseStamped()
+        self.odom = Odometry()
+        self.origin = PoseStamped()
+        
+        self.slave_ride_permission = True
+        
+        self.aruco = False
+        
+        self.start_rotation = False
+        self.rotation = False
+        
+        self.start_translation = False
+        self.translation = False
+        
+        self.target_angle = 0.0
+        
+        self.get_odom = False
+        self.get_origin = False
+        self.use_getting_origin = True
+        self.delta = 0.0
+        
+        self.start_dist = 0.0
+        
+        self.master_state = PoseStamped()
+        
+        msg = Twist()
+        msg.angular.z = 0.0
+        self.cmd_vel_pub.publish(msg)
+        
+    def aruco_callback(self, msg : Bool):
+        if msg.data:
+            self.aruco = True
+            print('ARUCO')
 
-    def controller_callback(self, goal_pose_: PoseStamped):
-        if self.slave_ride_permission:
-            goal_pose_in_so = self.map2bfs(goal_pose_)                                          # Преобразуем goal_pose в систему координат слейва
-            gp_point = self.pose_st2point_st(goal_pose_in_so, self.base_footprint_slave_link)   # Получаем точку goal_pose в координатах slave_odom
-            self.pose_from_points(self.slave_odom_point, gp_point)                              # Получаем вектор ориентации относительно slave_odom на точку goal_pose
+    def slave_origin_callback(self, msg : PoseStamped):
+        if self.use_getting_origin:
+            self.sl_odom_origin = msg
+            print('SLAVE ORIGIN', self.aruco)
+            self.use_getting_origin = False
+            self.get_origin = True
+            
+    def master_state_callback(self, msg : PoseStamped):
+        self.master_state = msg
+            
+    def delta_callback(self, msg : Float64):
+        self.delta = msg.data
+        
+    def start_rot_callback(self, msg : Bool):
+        time.sleep(1)
+        self.start_rotation = True
+        
+    def start_tran_callback(self, msg : Bool):
+        time.sleep(1)
+        self.start_translation = True
+            
+    def rotation_callback(self):
+        
+        msg = Twist()
+        
+        angle_error = abs(self.delta / 3.14 * 180)
+    
+        if angle_error > 20:
+            msg.angular.z = 0.3 if self.delta < 0 else -0.3
+        elif 10 < angle_error <= 20:
+            msg.angular.z = 0.1 if self.delta < 0 else -0.1
+        elif angle_error <= 10:
+            msg.angular.z = 0.05 if self.delta < 0 else -0.05
 
-            # Подсчет углов ориентации - - -
-            slave_yaw = self.getYaw(self.slave_odom.pose.pose.orientation)
-            gp_vector_yaw = self.getYaw(self.gp_vector.pose.orientation)
-            # - - - - - - - - - - - - - - - 
+        if self.start_rotation:
+            print('START_ROTATION')
+            self.start_rotation = False
+            self.rotation = True
+            
+            self.cmd_vel_pub.publish(msg)
+            return
 
-            if abs(slave_yaw - gp_vector_yaw) >= self.ANGLE_ERR:
-                self.turnLeft()
-            elif abs(self.slave_odom.pose.pose.position.x - self.gp_vector.pose.position.x) >= self.LINEAR_ERR or abs(self.slave_odom.pose.pose.position.y - self.gp_vector.pose.position.y) >= self.LINEAR_ERR:
-                self.moveForward()
+        if self.rotation:
+            self.get_logger().info(f'delta = {self.delta / 3.14 * 180}')
+            
+            if abs(self.delta / 3.14 * 180) < 20:
+                self.rotation = False
+                msg.angular.z = 0.0
+                self.cmd_vel_pub.publish(msg)
+                self.get_logger().info(f'ok')
+                rotated_msg = Bool()
+                rotated_msg.data = True
+                self.rot_pub.publish(rotated_msg)
             else:
-                self.reached_gp_flag_publisher.publish(True)      
+                self.cmd_vel_pub.publish(msg)
+                self.get_logger().info(f'not ok')
+                
+    def translation_callback(self):
+        
+        msg = Twist()
+        msg.linear.x = 0.2
+        
+        if self.start_translation:
+            print('START_TRANSLATION')
+            self.start_translation = False
+            self.translation = True
+            self.start_odom = self.odom
+            self.cmd_vel_pub.publish(msg)
+            dx = self.start_odom.pose.pose.position.x - self.master_state.pose.position.x + self.origin.pose.position.x
+            dy = self.start_odom.pose.pose.position.y - self.master_state.pose.position.y + self.origin.pose.position.y
+            self.start_dist = math.sqrt(dx ** 2 + dy ** 2)
+            return
 
-    def sl_odom_callback(self, msg: Odometry):
-        self.slave_odom = msg
-
-        point_st = PointStamped()
-        point_st.header.frame_id = self.base_footprint_slave
-        point_st.header.stamp = self.get_clock().now().to_msg()
-
-        point_st.point.x = self.slave_odom.pose.pose.position.x
-        point_st.point.y = self.slave_odom.pose.pose.position.y
-        point_st.point.z = self.slave_odom.pose.pose.position.z
-
-        self.slave_odom_point = point_st
-    
-    def mas_odom_callback(self, msg):
-        self.master_odom = msg
-    
-    def slave_origin_callback(self, msg):
-        self.sl_odom_origin = msg
+        if self.translation:
+            dx = self.origin.pose.position.x - self.odom.pose.pose.position.x
+            dy = self.origin.pose.position.y - self.odom.pose.pose.position.y
+            dist = math.sqrt(dx ** 2 + dy ** 2)
+            self.get_logger().info(f'dist = {dist}, start_dist = {self.start_dist}')
+            
+            if dist > self.start_dist:
+                self.translation = False
+                msg.linear.x = 0.0
+                self.cmd_vel_pub.publish(msg)
+                self.get_logger().info(f'ok')
+                translated_msg = Bool()
+                translated_msg.data = True
+                self.tran_pub.publish(translated_msg)
+            else:
+                self.cmd_vel_pub.publish(msg)
+                self.get_logger().info(f'not ok')
+        
+    def odom_callback(self, msg: Odometry):
+        self.odom = msg
+        self.get_odom = True
 
     def slave_ride_permission_callback(self, msg):
         self.slave_ride_permission = msg
 
-    def pose_from_points(self, point1: PointStamped, point2: PointStamped) -> PoseStamped:  # Рассчет в 'map', возвращает в системе координат 'slave_odom'
-        # Вычисляем разницу координат между точками
-        dx = point2.point.x - point1.point.x
-        dy = point2.point.y - point1.point.y
-        dz = point2.point.z - point1.point.z
+
+def euler_from_quaternion(x, y, z, w):
+            """
+            Convert a quaternion into euler angles (roll, pitch, yaw)
+            roll is rotation around x in radians (counterclockwise)
+            pitch is rotation around y in radians (counterclockwise)
+            yaw is rotation around z in radians (counterclockwise)
+            """
+            t0 = +2.0 * (w * x + y * z)
+            t1 = +1.0 - 2.0 * (x * x + y * y)
+            roll_x = math.atan2(t0, t1)
         
-        # Вычисляем углы yaw и pitch для ориентации
-        yaw = math.atan2(dy, dx)       # Вращение вокруг оси z
-        pitch = math.atan2(dz, math.sqrt(dx**2 + dy**2))  # Вращение вокруг оси y
-        roll = 0.0  # Вращение вокруг оси x (обычно для таких задач не требуется)
-
-        # Преобразуем углы yaw, pitch и roll в кватернион
-        q = tf_transformations.quaternion_from_euler(roll, pitch, yaw)
+            t2 = +2.0 * (w * y - z * x)
+            t2 = +1.0 if t2 > +1.0 else t2
+            t2 = -1.0 if t2 < -1.0 else t2
+            pitch_y = math.asin(t2)
         
-        # Создаем объект PoseStamped
-        pose = PoseStamped()
-        pose.header = Header()
-        pose.header.stamp = rclpy.time.Time().to_msg()  # Текущее время ROS2
-        pose.header.frame_id = self.map_link  # Укажите нужную систему координат
-
-        # Устанавливаем позицию как point1
-        pose.pose.position = point1
-
-        # Устанавливаем ориентацию в кватернион
-        pose.pose.orientation = Quaternion(x=q[0], y=q[1], z=q[2], w=q[3])
-
-        # pose = self.map2bfs(pose)  # Перевод позиции в систему координат слейва
-
-        self.gp_vector = pose
-
-    def map2bfs(self, pose_: PoseStamped):
-        pose_.pose.position.x = pose_.pose.position.x - self.sl_odom_origin.pose.position.x
-        pose_.pose.position.y = pose_.pose.position.y - self.sl_odom_origin.pose.position.y
-
-        new_orient = tf_transformations.euler_from_quaternion(pose_.pose.orientation)
-        sl_odom_orient = tf_transformations.euler_from_quaternion(self.sl_odom_origin.pose.orientation)
-        new_orient[0] = new_orient[0] - sl_odom_orient[0]
-        new_orient[0] = new_orient[1] - sl_odom_orient[1]
-        new_orient[0] = new_orient[2] - sl_odom_orient[2]
-        pose_.pose.orientation = tf_transformations.quaternion_from_euler(new_orient)
-
-        return pose_
-
-    def getYaw(q_orient: Quaternion):
-        orient = tf_transformations.euler_from_quaternion(q_orient)
-        yaw = orient.z
-
-        return yaw
-
-    def moveForward(self):
-        speed = Twist()
-        speed.angular = 0.0
-        speed.linear = self.max_linear_vel
-
-        self.vel_publisher.publish(speed)
+            t3 = +2.0 * (w * z + x * y)
+            t4 = +1.0 - 2.0 * (y * y + z * z)
+            yaw_z = math.atan2(t3, t4)
         
-    def turnLeft(self):
-        speed = Twist()
-        speed.linear = 0.0
-        speed.angular = -self.max_angular_vel
-
-        self.vel_publisher.publish(speed)
-
-    def publish_point(self, p : Point, frame_id):
-        point_msg = PointStamped()
-
-        point_msg.header.stamp = self.get_clock().now().to_msg()
-        point_msg.header.frame_id = frame_id
+            return [roll_x, pitch_y, yaw_z] # in radians
         
-        point_msg.point.x = p.x
-        point_msg.point.y = p.y
-        point_msg.point.z = p.z
         
-        self.point_publisher.publish(point_msg)
+def quaternion_from_euler(ai, aj, ak):
+    ai /= 2.0
+    aj /= 2.0
+    ak /= 2.0
+    ci = math.cos(ai)
+    si = math.sin(ai)
+    cj = math.cos(aj)
+    sj = math.sin(aj)
+    ck = math.cos(ak)
+    sk = math.sin(ak)
+    cc = ci*ck
+    cs = ci*sk
+    sc = si*ck
+    ss = si*sk
 
-    def pose_st2point_st(self, pose_: PoseStamped, frame_id) -> PointStamped:
-        point_st = PointStamped()
-        point_st.header.frame_id = frame_id
-        point_st.header.stamp = self.get_clock().now().to_msg()
+    q = np.empty((4, ))
+    q[0] = cj*sc - sj*cs
+    q[1] = cj*ss + sj*cc
+    q[2] = cj*cs - sj*sc
+    q[3] = cj*cc + sj*ss
 
-        point_st.point.x = pose_.pose.position.x
-        point_st.point.y = pose_.pose.position.y
-        point_st.point.z = pose_.pose.position.z
+    return q
 
-        return point_st
-
-
-def transform_pose_stamped(pose: PoseStamped, transform: TransformStamped) -> PoseStamped:
-    # Извлекаем позицию и ориентацию из PoseStamped
-    position = pose.pose.position
-    orientation = pose.pose.orientation
-    
-    # Преобразуем позицию в массив
-    p = [position.x, position.y, position.z]
-    
-    # Получаем кватернион из ориентации
-    q = [orientation.x, orientation.y, orientation.z, orientation.w]
-    
-    # Получаем матрицы трансформации
-    translation_matrix = tf_transformations.translation_matrix([transform.transform.translation.x,
-                                                                transform.transform.translation.y,
-                                                                transform.transform.translation.z])
-    rotation_matrix = tf_transformations.quaternion_matrix([transform.transform.rotation.x,
-                                                            transform.transform.rotation.y,
-                                                            transform.transform.rotation.z,
-                                                            transform.transform.rotation.w])
-    
-    # Объединяем матрицы
-    transform_matrix = tf_transformations.concatenate_matrices(translation_matrix, rotation_matrix)
-    
-    # Преобразуем позицию в гомогенные координаты
-    p_homogeneous = tf_transformations.concatenate_matrices(transform_matrix, tf_transformations.translation_matrix(p + [1.0]))
-    
-    # Извлекаем новую позицию из гомогенных координат
-    new_position = p_homogeneous[:3, 3]
-    
-    # Преобразуем кватернион в новую ориентацию
-    new_orientation = tf_transformations.quaternion_multiply(q, [transform.transform.rotation.x,
-                                                                 transform.transform.rotation.y,
-                                                                 transform.transform.rotation.z,
-                                                                 transform.transform.rotation.w])
-    
-    # Создаем новый PoseStamped
-    new_pose = PoseStamped()
-    new_pose.header = pose.header  # Копируем заголовок
-    new_pose.pose.position.x = new_position[0]
-    new_pose.pose.position.y = new_position[1]
-    new_pose.pose.position.z = new_position[2]
-    new_pose.pose.orientation.x = new_orientation[0]
-    new_pose.pose.orientation.y = new_orientation[1]
-    new_pose.pose.orientation.z = new_orientation[2]
-    new_pose.pose.orientation.w = new_orientation[3]
-    
-    return new_pose
 
 def main(args=None):
     rclpy.init(args=args)
